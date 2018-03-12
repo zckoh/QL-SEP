a9d63d1759315422253689e66a1f902de553b53b
Zheng Cong <zckoh96@gmail.com>
2018-03-12 12:31:14 +0000

    Include alpha-adapt with QLSEP + sharing every slot (LCD)

--- method 4-sharing QLSEP parameters/Sharing Parameters every slot + alpha-adapt (QLSEP,Actual LCD).py
+++ method 4-sharing QLSEP parameters/Sharing Parameters every slot + alpha-adapt (QLSEP,Actual LCD).py
@@ -1,14 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 10 14:05:16 2018
+Created on Mon Mar 12 12:18:21 2018
 
 QLSEP model (With 1 node sharing to the other every slot)
-(Actual HCD Data)
+(Actual LCD Data)
 +
 Alpha - adapt
 
 @author: zckoh
 """
+
 
 
 import numpy as np
@@ -25,7 +26,7 @@ lux_B1 = []
 slot = 60
 
 for i in range(1,21):
-    with open("./../highly correlated data/Box 1/day%s.txt" %i , 'r') as f:
+    with open("./../Less Correlated/Box 1/day%s.txt" %i , 'r') as f:
         fifthlines = itertools.islice(f, 0, None, slot)
         for lines in fifthlines:
             tmp.append(lines)
@@ -37,7 +38,7 @@ days = len(lux_B1)
 lux_B2 = []
 
 for i in range(1,21):
-    with open("./../highly correlated data/Box 2/day%s.txt" %i , 'r') as f:
+    with open("./../Less Correlated/Box 2/day%s.txt" %i , 'r') as f:
         fifthlines = itertools.islice(f, 0, None, slot)
         for lines in fifthlines:
             tmp.append(lines)
@@ -56,7 +57,7 @@ for x in range(0,days):
     for y in range(0,1440/slot):
         """Node 1 predicting"""
         node1.alpha_adapt(x,y,(np.amax(lux_B1[x])*0.03),lux_B1[x][y-1])
-        node1.a2 = 0.5*node2.a2 + 0.5*node1.a2
+        node1.a2 = 0.3*node2.a2 + 0.7*node1.a2
         node1.EWMA_dynamic(x,y,lux_B1[x-1][y])
         node1.Calculate_PER(x,y,lux_B1[x][y-1],(np.amax(lux_B1[x])*0.03))
         node1.Q_val_update(x,y)
@@ -69,13 +70,9 @@ for x in range(0,days):
         node2.Calculate_PER(x,y,lux_B2[x][y-1],(np.amax(lux_B2[x])*0.03))
         node2.Q_val_update(x,y)
         node2.QLSEP_prediction(x,y)
-#        if(x==15):
-#            print "node 1:%s\tnode 2:%s"% (node1.q_values[y],node2.q_values[y])
-
+        
         #Node 2 share to node 1
-        node1.q_values[y] = 0.5*node1.q_values[y] + 0.5*node2.q_values[y]
-#        if(x==15):
-#            print "node 1 updated:%s\t"%node1.q_values[y]
+        node1.q_values[y] = 0.3*node1.q_values[y] + 0.7*node2.q_values[y]
         if(y==12):
             n1_q_val.append(node1.q_values[y])
             n2_q_val.append(node2.q_values[y])
