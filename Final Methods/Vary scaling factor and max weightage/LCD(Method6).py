5e45e7a79c21798035d91b9fbce3d76a5f950c2e
Zheng Cong <zckoh96@gmail.com>
2018-04-08 23:26:08 +0100

    Stick with initial M6 as planned

--- Final Methods/Vary scaling factor and max weightage/LCD(Method6).py
+++ Final Methods/Vary scaling factor and max weightage/LCD(Method6).py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Apr 05 22:01:31 2018
+Created on Sun Apr 08 21:33:40 2018
 
 @author: zckoh
 """
@@ -10,41 +10,55 @@ Created on Thu Apr 05 22:01:31 2018
 import numpy as np
 import matplotlib.pyplot as plt
 import itertools
+import matplotlib.mlab as mlab
+from mpl_toolkits.mplot3d import axes3d
+
 execfile("./../../QLSEP_class.py")
 np.set_printoptions(threshold=np.nan)
+np.set_printoptions(precision=4)
 
 class prettyfloat(float):
     def __repr__(self):
         return "%0.4f" % self
 
+
 #Get the original 48 slots data
-slot = 30
-day_counter = 1
-lux_original = []
-slot_true = 1
-lux = []
 tmp = []
-with open("./../../NREL_data/20160901.csv", 'r') as f:
-    fifthlines = itertools.islice(f, 0, None, slot)
-    for lines in fifthlines:
-        tmp.append(lines.split(',')[2])
-        if(day_counter == (1440/slot)):
-            day_counter = 0
-            tmp = [w.replace('\n', '') for w in tmp]
-            lux_original.append([float(i) for i in tmp])
-            tmp = []            
-        day_counter += 1
-
-days = len(lux_original)
-
-#split into 2 arrays
 lux_B1 = []
+slot = 30
+"""getting data"""
+for i in range(1,21):
+    with open("./../../Less Correlated/Box 1/day%s.txt" %i , 'r') as f:
+        fifthlines = itertools.islice(f, 0, None, slot)
+        for lines in fifthlines:
+            tmp.append(lines)
+        tmp = [w.replace('\n', '') for w in tmp]
+    lux_B1.append([float(i) for i in tmp])
+    tmp = []
+days = len(lux_B1)
+
 lux_B2 = []
-for x in range(len(lux_original)):
-    even = lux_original[x][0:][::2]
-    lux_B1.append(even)
-    odd = lux_original[x][1::][::2]
-    lux_B2.append(odd)
+
+for i in range(1,21):
+    with open("./../../Less Correlated/Box 2/day%s.txt" %i , 'r') as f:
+        fifthlines = itertools.islice(f, 0, None, slot)
+        for lines in fifthlines:
+            tmp.append(lines)
+        tmp = [w.replace('\n', '') for w in tmp]
+    lux_B2.append([float(i) for i in tmp])
+    tmp = []
+
+days = len(lux_B1)
+
+
+#split into 2 arrays  (Box 1)
+lux_B1_even = []
+lux_B2_odd = []
+for x in range(len(lux_B1)):
+    even = lux_B1[x][0:][::2]
+    lux_B1_even.append(even)
+    odd = lux_B2[x][1::][::2]
+    lux_B2_odd.append(odd)
     
     
 
@@ -52,8 +66,9 @@ for x in range(len(lux_original)):
 
 
 
-m_w_lst = np.linspace(0.1,1, num = 22)
+m_w_lst = np.linspace(0,1, num = 22)
 MAPE_QLSEP_lst = []
+
 
 for j in range(len(m_w_lst)):
     param = m_w_lst[j]
@@ -61,16 +76,16 @@ for j in range(len(m_w_lst)):
         max_weightage = m_w_lst[k]
         node1 = QLSEP_node(0.001,0.1,3,60,days,50)
         node2 = QLSEP_node(0.001,0.1,3,60,days,50)
-        for x in range(0,days): 
+        for x in range(0,days):
             for y in range(0,1440/60):
                 #Node1 updates its alpha & predicts for next slot
-                node1.alpha_adapt(x,y,(np.amax(lux_B1[x])*0.03),lux_B1[x][y-1])
-                node1.Calculate_PER(x,y,lux_B1[x][y-1],(np.amax(lux_B1[x])*0.03))
+                node1.alpha_adapt(x,y,(np.amax(lux_B1_even[x])*0.03),lux_B1_even[x][y-1])
+                node1.Calculate_PER(x,y,lux_B1_even[x][y-1],(np.amax(lux_B1_even[x])*0.03))
 
                 #Node2 updates its alpha & predicts for next slot
-                node2.alpha_adapt(x,y,(np.amax(lux_B2[x])*0.03),lux_B2[x][y-1])
-                node2.EWMA_dynamic(x,y,lux_B2[x-1][y])
-                node2.Calculate_PER(x,y,lux_B2[x][y-1],(np.amax(lux_B2[x])*0.03))
+                node2.alpha_adapt(x,y,(np.amax(lux_B2_odd[x])*0.03),lux_B2_odd[x][y-1])
+                node2.EWMA_dynamic(x,y,lux_B2_odd[x-1][y])
+                node2.Calculate_PER(x,y,lux_B2_odd[x][y-1],(np.amax(lux_B2_odd[x])*0.03))
                 node2.Q_val_update(x,y)
                 node2.QLSEP_prediction(x,y)
         
@@ -81,15 +96,15 @@ for j in range(len(m_w_lst)):
                     ratio=0
                 node1.a2=(max_weightage-ratio)*node2.a2+(1-max_weightage+ratio)*node1.a2
 
-                node1.EWMA_dynamic(x,y,lux_B1[x-1][y])
+                node1.EWMA_dynamic(x,y,lux_B1_even[x-1][y])
                 node1.Q_val_update(x,y)
                 node1.QLSEP_prediction(x,y)
                 
-        [mape_b1_QLSEP, no_b1_QLSEP] = MAPE_overall(lux_B1,node1.QLSEP_val,days)
-        [mape_b1_EWMA, no_b1_EWMA] = MAPE_overall(lux_B1,node1.EWMA_val,days)
+        [mape_b1_QLSEP, no_b1_QLSEP] = MAPE_overall(lux_B1_even,node1.QLSEP_val,days)
+        [mape_b1_EWMA, no_b1_EWMA] = MAPE_overall(lux_B1_even,node1.EWMA_val,days)
 
-        [mape_b2_QLSEP, no_b2_QLSEP] = MAPE_overall(lux_B2,node2.QLSEP_val,days)
-        [mape_b2_EWMA, no_b2_EWMA] = MAPE_overall(lux_B2,node2.EWMA_val,days)
+        [mape_b2_QLSEP, no_b2_QLSEP] = MAPE_overall(lux_B2_odd,node2.QLSEP_val,days)
+        [mape_b2_EWMA, no_b2_EWMA] = MAPE_overall(lux_B2_odd,node2.EWMA_val,days)
         
         MAPE_QLSEP_lst.append(mape_b1_QLSEP)
 
@@ -130,11 +145,11 @@ ax.set_xlabel(r'$W_{max}$')
 ax.set_zlabel('MAPE')
 ax.grid()
 #plt.title(r'How MAPE varies w.r.t max weightage and scaling factor')
-plt.savefig('MAPE_vs_C_W(Method6).png',dpi=600)
-
+#plt.savefig('MAPE_vs_C_W(Method6).png',dpi=600)
 
 
 # =============================================================================
+# 
 # plt.figure(1)
 # fig, ax = plt.subplots(figsize=(7.5,4))
 # ax.plot(m_w_lst,MAPE_QLSEP_lst,'b')
